62644fd82b9a69c1f0181afe2dc778562a80a881
Yu Yejin <postpone.jin@gmail.com>
2022-07-13 08:28:30 +0900

    Using prefix sum

--- boj/subtask/10539.py
+++ boj/subtask/10539.py
@@ -1,9 +1,12 @@
 N, B = int(input()), list(map(int, input().split()))
 
 A = [B[0]]
+# A = [0 for i in range[B]]
+# A[0] = B[0]
 
 for i in range(1, N):
     A.append(B[i]*(i+1) - sum(A))
+    # A[i] = B[i]*(i+1) - sum(A)
 
 for i in A:
     print(i, end=' ')
