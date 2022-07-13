e1b3ef01a69b712cde0527061939be8062836f5d
Yu Yejin <postpone.jin@gmail.com>
2022-07-13 09:08:06 +0900

    Handling string character arrays

---
+++ boj/subtask/17269.py
@@ -0,0 +1,13 @@
+N, M = map(int, input().split())
+A, B = input().split()
+
+alp = [3, 2, 1, 2, 4, 3, 1, 1, 3, 1, 3, 2, 1, 2, 2, 2, 1, 2, 1, 1, 1, 2, 2, 1]
+
+AB = ''
+min_len = min(N, M)
+for i in range(min_len):
+    AB += A[i] + B[i]
+
+AB += A[min_len:] + B[min_len:]
+
+lst = [alp[ord(i)-ord('A')] for i in AB]
