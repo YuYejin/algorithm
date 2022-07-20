93c7f559b8c203c3442896b0b1b23e7792a11a6d
Yu Yejin <postpone.jin@gmail.com>
2022-07-21 07:53:57 +0900

    comprehension dictionary

--- others/comprehension.py
+++ others/comprehension.py
@@ -29,3 +29,21 @@ print(dinner) # [('쌈밥', '사과'), ('쌈밥', '아이스크림'), ('쌈밥',
 # ------------------------------------------------
 lst = [x for x in range(10) if x < 5 if x % 2 == 0] # if문 중복
 print(lst) # [0, 2, 4]
+
+# < dict comprehension >
+# list comprehension에서 {}를 이용하면 set comprehension이 됨
+set = {x+y for x in range(10) for y in range(10)}
+print(set) # {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18}
+# ------------------------------------------------
+# {}를 이용하면서 key:value 형태로 내용을 채우면 dictionary comprehension이 됨
+students = ['철수', '영희', '길동', '순희']
+print({student: 0 for student in students}) # {'철수': 0, '영희': 0, '길동': 0, '순희': 0}
+
+# 전학간 학생 성적 제거
+scores = {'철수':50, '영희':80, '길동':90, '순희':60, '전학생':100}
+scores = {name: score for name, score in scores.items() if name != '전학생'}
+print(scores) # {'철수': 50, '영희': 80, '길동': 90, '순희': 60}
+
+# 통과, 불통과
+grades = {name: 'PASS' if value > 70 else 'No-PASS' for name, value in scores.items()}
+print(grades) # {'철수': 'No-PASS', '영희': 'PASS', '길동': 'PASS', '순희': 'No-PASS'}
