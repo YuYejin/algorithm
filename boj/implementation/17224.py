5b1099c00d19d6904e94d12994ec37ff1e8f5b1b
Yu Yejin <postpone.jin@gmail.com>
2022-07-23 07:58:27 +0900

    long fingerprint exercises

--- boj/implementation/17224.py
+++ boj/implementation/17224.py
@@ -1,3 +1,18 @@
-N, L, K = map(int, input().split())
+N, L, K = map(int, input().split()) # 문제수, 역량, 최대 개수
 
+easy, hard = 0, 0 # 풀 수 있는 쉬운 문제 수와 어려운 문제 수
 
+for i in range(N):
+    sub1, sub2 = map(int, input().split())
+    if sub2 <= L:
+        hard += 1
+    elif sub1 <= L:
+        easy += 1
+
+# hard 문제
+ans = min(hard, K) * 140
+# easy 문제
+if hard < K:
+    ans += min(K-hard, easy) * 100
+
+print(ans)
